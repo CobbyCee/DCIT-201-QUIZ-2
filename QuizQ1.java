c01914371c21d68da7311e1c3d6c1c61ca5f3398
CobbyCee <princenarteycobb@gmail.com>
2026-02-05 11:49:47 +0000

    Create QuizQ1.java

---
+++ QuizQ1.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        executeGradeReport(0);
+    }
+
+    public static void executeGradeReport(double score) {
+
+        if (!validateScore(score)) {
+            System.out.println("Invalid Score");
+            return;
+        }
+
+        char grade = calculateLetterGrade(score);
+
+        displayPerformanceMessage(grade);
+    }
+    public static boolean validateScore(double score) {
+        return true;
+    }
+    public static char calculateLetterGrade(double score) {
+        return 'i';
+    }
+    public static void displayPerformanceMessage(char grade) {
+        System.out.println("Final feedback displayed");
+    }
+}
